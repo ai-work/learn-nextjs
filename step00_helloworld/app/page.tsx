8f6649f5310b813a956b66cd6b6ce81bbe4ce11b
Asif Iqbal <asifgvs@gmail.com>
2023-04-03 14:13:37 +0200

    kysely neon access in client side component

--- step00_helloworld/app/page.tsx
+++ step00_helloworld/app/page.tsx
@@ -1,11 +1,37 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-//import styles from './page.module.css'
+'use client'
 
-const inter = Inter({ subsets: ['latin'] })
+import {
+  Kysely,
+  Generated,
+} from 'kysely';
 
-export default function Home() {
+import { NeonDialect } from "kysely-neon";
+
+interface playing_with_neon_Table {
+  id: Generated<number>
+  name: string
+  value: number
+}
+
+interface Database {
+  playing_with_neon: playing_with_neon_Table
+}
+
+export default async function Home() {
+  const db = new Kysely<Database>({
+    dialect: new NeonDialect({
+      connectionString: "NEON_DATABASE_URL=postgres://ai-work:rxia5S1LZMjq@lingering-flower-232313.eu-central-1.aws.neon.tech/neondb",
+    }),
+  });
+  
+  const result = await db
+  .selectFrom("playing_with_neon")
+  .selectAll()
+  .execute()
+  
+  console.log("backend result ", result);
+
   return (
-    <div>Hello Next.js World from Asif Iqbal</div>
+    <div>result</div>
   )
 }
