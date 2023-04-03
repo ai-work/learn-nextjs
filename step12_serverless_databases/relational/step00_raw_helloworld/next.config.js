4ad5405ec6f930a60edd46a1c46b71e8fde5f9d2
Asif Iqbal <asifgvs@gmail.com>
2023-04-03 13:01:11 +0200

    hardcoded neon creds removed. define as vercel env vars

--- step12_serverless_databases/relational/step00_raw_helloworld/next.config.js
+++ step12_serverless_databases/relational/step00_raw_helloworld/next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-  },
-  env: {
-    PGHOST: 'ep-little-3191.us-east-2.aws.neon.tech',
-    PGDATABASE: 'neondb',
-    PGUSER:'ziaukhan',
-    PGPASSWORD:'oCRid75Qvn'
   }
 }
 
